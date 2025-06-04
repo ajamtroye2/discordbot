e683a8a1b351a60c21650345ff68b036570bde15
Swann Grandin <108907005+ajamtroye2@users.noreply.github.com>
2025-06-04 17:16:14 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,36 @@
 import os
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix="!")
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} est connecté.")
+    print(f"{bot.user} est connecté !")
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("❗ Il manque un argument.")
+    elif isinstance(error, commands.CommandNotFound):
+        await ctx.send("❌ Cette commande n'existe pas.")
+    else:
+        await ctx.send(f"Erreur inconnue : {str(error)}")
+
+initial_extensions = [
+    "cogs.hello",
+    "cogs.maths",
+    "cogs.api"
+]
+
+for ext in initial_extensions:
+    bot.load_extension(ext)
 
 bot.run(TOKEN)
