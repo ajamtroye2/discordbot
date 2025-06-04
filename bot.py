e664d67337cc63054ecb20db82bdaccb11523627
Swann Grandin <108907005+ajamtroye2@users.noreply.github.com>
2025-06-04 17:01:19 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} est connecté.")
+
+bot.run(TOKEN)
